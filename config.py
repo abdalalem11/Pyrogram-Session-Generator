605baa89fb74dc52e6256d7ad22caff70650282e
abd <bwdalmshrqy455@gmail.com>
2026-07-23 03:20:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ from os import getenv
 API_ID = int(getenv("API_ID", "38532428"))
 API_HASH = getenv("API_HASH", "bd13b721c96184649dbbce14de78147d")
 BOT_TOKEN = getenv("BOT_TOKEN", "8770149502:AAFkmj14adoaUHCFHXcBO9aNwjjDdpcfuLY")
-LOG_GROUP = int(getenv("LOG_GROUP", "@FORWARD_CHAT_ID"))
+LOG_GROUP = int(getenv("LOG_GROUP", "4463855635"))
