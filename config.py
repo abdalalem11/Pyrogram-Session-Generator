c4d48e1e4a1f756249468547b0fe0e0cb621108f
abd <bwdalmshrqy455@gmail.com>
2026-07-23 08:10:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", 38532428))
 API_HASH = getenv("API_HASH", "bd13b721c96184649dbbce14de78147d")
-BOT_TOKEN = getenv("BOT_TOKEN", "8770149502:AAFkmj14adoaUHCFHXcBO9aNwjjDdpcfuLY")
+BOT_TOKEN = getenv("BOT_TOKEN", "8899112097:AAFqGE9YrChxbXPbebPRIaxsCcFbzd-HQ1s")
 LOG_GROUP = int(getenv("LOG_GROUP", -1004463855635))
