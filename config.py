f51ed39638109852ba96d34e15eb865b7aa8fe54
abd <bwdalmshrqy455@gmail.com>
2026-07-23 03:08:04 +0300

    Set default values for API_ID, API_HASH, and BOT_TOKEN
    Updated environment variable defaults for API credentials.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-LOG_GROUP = int(getenv("LOG_GROUP", ""))
+API_ID = int(getenv("API_ID", "38532428"))
+API_HASH = getenv("API_HASH", "bd13b721c96184649dbbce14de78147d")
+BOT_TOKEN = getenv("BOT_TOKEN", "8770149502:AAFkmj14adoaUHCFHXcBO9aNwjjDdpcfuLY")
+LOG_GROUP = int(getenv("LOG_GROUP", "@FORWARD_CHAT_ID"))
