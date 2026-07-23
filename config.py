daf1a09c80992ba2bf0b408f0fb3e33603b0230f
abd <bwdalmshrqy455@gmail.com>
2026-07-23 05:26:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 # devggn
-
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
 from os import getenv
@@ -7,4 +6,4 @@ from os import getenv
 API_ID = int(getenv("API_ID", 38532428))
 API_HASH = getenv("API_HASH", "bd13b721c96184649dbbce14de78147d")
 BOT_TOKEN = getenv("BOT_TOKEN", "8770149502:AAFkmj14adoaUHCFHXcBO9aNwjjDdpcfuLY")
-LOG_GROUP = int(getenv("LOG_GROUP", -1004463855635))  # ✅ التعديل هنا
+LOG_GROUP = int(getenv("LOG_GROUP", -1004463855635))
