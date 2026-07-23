3738d8c94af8756248e3c9dd677dda4c717d2b61
abd <bwdalmshrqy455@gmail.com>
2026-07-23 03:53:02 +0300

    Update LOG_GROUP to use a public group ID
    Change LOG_GROUP environment variable to a public group ID.

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ from os import getenv
 API_ID = int(getenv("API_ID", "38532428"))
 API_HASH = getenv("API_HASH", "bd13b721c96184649dbbce14de78147d")
 BOT_TOKEN = getenv("BOT_TOKEN", "8770149502:AAFkmj14adoaUHCFHXcBO9aNwjjDdpcfuLY")
-LOG_GROUP = int(getenv("LOG_GROUP", "-1004463855635"))  # أضف -100 إذا كانت خاصة
+LOG_GROUP = int(getenv("LOG_GROUP", "4463855635"))
